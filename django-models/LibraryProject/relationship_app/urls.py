b119f4b0f9583d2340f739242fd878e7365b1818
nobbonsu <nobbonsu440@gmail.com>
2025-11-13 14:10:21 +0000

    implementing custom permission in django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,6 +3,15 @@ from django.contrib.auth.views import LogoutView, LoginView
 from . import views
 
 urlpatterns = [
+    # List all books
+    path('books/', views.list_books, name='list_books'),
+    # Add a new book
+    path('add_book/', views.add_book, name='add_book'),
+    # Edit an existing book by its primary key (book_id)
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    # Delete an existing book by its primary key (book_id)
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
+
     path('display/', views.display_all, name='display_all'),
     path('books/', views.list_books, name='list_books'),  # Function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view()), # Class-based view
