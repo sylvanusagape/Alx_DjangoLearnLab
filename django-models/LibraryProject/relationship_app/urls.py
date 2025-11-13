dc3e62f519f80a822b4e1a0de01dd06fa461ba5c
nobbonsu <nobbonsu440@gmail.com>
2025-11-13 11:17:10 +0000

    User registration, login and logout functionality

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView, LoginView
 from . import views
 from .views import list_books
 
 urlpatterns = [
     path('display/', views.display_all, name='display_all'),
     path('books/', views.list_books, name='list_books'),  # Function-based view
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), # Class-based view
+    path('library/<int:pk>/', views.LibraryDetailView.as_view()), # Class-based view
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'), name='library_detail')
+    path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html"), LoginView.as_view(template_name="relationship_app/login.html"))
 ]
