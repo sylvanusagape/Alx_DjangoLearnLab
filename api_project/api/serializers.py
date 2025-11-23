68beeb4686e950a3b26d148b2e9afeba89eaf3ee
nobbonsu <nobbonsu440@gmail.com>
2025-11-23 17:23:39 +0000

    serializers update

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,6 +1,6 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Book
 
-class BookSerializer(ModelSerializer):
+class BookSerializer(serializers.ModelSerializer):
     model = Book
     fields = '__all__'
