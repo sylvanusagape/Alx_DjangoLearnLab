2debf54a289c635c71c8666578868617893d8278
sylvanusagape <slywin9495@gmail.com>
2025-12-14 04:19:06 +0000

    Project 0: Initial Django setup with custom user model and token authentication

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+def register_view(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("profile")
+        else:
+            messages.error(request, "Unsuccessful registration. Invalid information.")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "blog/register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("profile")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+    return render(request, "blog/login.html", {"form": form})
+
+@login_required
+def profile_view(request):
+    if request.method == "POST":
+        request.user.email = request.POST.get("email", request.user.email)
+        request.user.save()
+        messages.success(request, "Profile updated successfully!")
+    return render(request, "blog/profile.html", {"user": request.user})
+
