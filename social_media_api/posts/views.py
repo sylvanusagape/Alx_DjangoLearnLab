38ee02b939b5b9ed0a31f64c4887312b76857a76
sylvanusagape <slywin9495@gmail.com>
2025-12-14 09:13:06 +0000

    Dealing with errors

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,15 +1,22 @@
 from rest_framework import viewsets, permissions, generics
+from rest_framework.exceptions import PermissionDenied
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
-
+# Custom permission: only owners can edit/delete
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to allow only owners to edit or delete their objects.
+    """
+
     def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed for any request
         if request.method in permissions.SAFE_METHODS:
             return True
+        # Write permissions allowed only to the owner
         return obj.user == request.user
 
-
+# Post CRUD
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
@@ -18,7 +25,7 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-
+# Comment CRUD
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
@@ -27,13 +34,16 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-
+# Feed view
 class FeedView(generics.ListAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        following_users = self.request.user.following.all()
+        user = self.request.user
+        following_users = user.following.all()
+
+        # Correct field for Post model: 'user'
         return Post.objects.filter(
             user__in=following_users
         ).order_by('-created_at')
